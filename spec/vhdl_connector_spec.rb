c6ad166bcd8b2c4eda265447bfba620698d111c4
Tomoya Chiba <tomo.asleep@gmail.com>
2013-12-23 00:18:51 +0900

    Remove vhdl_connector_spec's unused spec

--- spec/vhdl_connector_spec.rb
+++ spec/vhdl_connector_spec.rb
@@ -4,8 +4,4 @@ describe VhdlConnector do
   it 'should have a version number' do
     VhdlConnector::VERSION.should_not be_nil
   end
-
-  it 'should do something useful' do
-    false.should be_true
-  end
 end
