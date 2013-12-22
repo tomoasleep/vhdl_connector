24880c02d19f77094e1a8725da321cdcf2fd29f7
Tomoya Chiba <tomo.asleep@gmail.com>
2013-12-22 21:43:00 +0900

    Fix helper's templates path

--- lib/vhdl_connector/helper.rb
+++ lib/vhdl_connector/helper.rb
@@ -1,8 +1,8 @@
 module VhdlConnector::Helper
   RootPath = File.expand_path('../../..', __FILE__)
-  TemplatePath = File.expand_path('./template', RootPath)
+  TemplatePath = File.expand_path('./templates', RootPath)
 
   def template_path(path)
-    File.expand_path(path, Helper::TemplatePath)
+    File.expand_path(path, TemplatePath)
   end
 end
