576fe249d099544665962a230c322986ad62f154
Tomoya Chiba <tomo.asleep@gmail.com>
2013-12-22 18:27:34 +0900

    Implement top module

--- lib/vhdl_connector.rb
+++ lib/vhdl_connector.rb
@@ -1,5 +1,18 @@
 require "vhdl_connector/version"
+require "vhdl_connector/helper"
+require "vhdl_connector/presenters"
+require "vhdl_connector/view"
+
+require "vhdl_connector/entity_wrapper"
+require "vhdl_connector/generic_wrapper"
+require "vhdl_connector/port_wrapper"
 
 module VhdlConnector
-  # Your code goes here...
+  def parse_entity(vhdl_str)
+    EntityWrapper.new(VHDL_Parser.parse(vhdl_str))
+  end
+
+  def parse_entity_file(path)
+    EntityWrapper.new(VHDL_Parser.parse_file(path))
+  end
 end
