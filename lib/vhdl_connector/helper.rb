aefdf3c0a3d5052e9e7f26aca4d151dc5c55cd8e
Tomoya Chiba <tomo.asleep@gmail.com>
2013-12-22 23:07:51 +0900

    Implement arguments helper

--- lib/vhdl_connector/helper.rb
+++ lib/vhdl_connector/helper.rb
@@ -5,4 +5,12 @@ module VhdlConnector::Helper
   def template_path(path)
     File.expand_path(path, TemplatePath)
   end
+
+  def options_of_args(args)
+    args.reduce(Hash.new) { |arg, options| args.is_a? Hash ? options.merge(arg) : options }
+  end
+
+  def args_without_options(args)
+    args.select { |arg| not args.is_a? Hash }
+  end
 end
