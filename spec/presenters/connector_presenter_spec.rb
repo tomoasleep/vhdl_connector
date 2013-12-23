238fc8c542a5fad000f54abe73d227b6b052b52d
Tomoya Chiba <tomo.asleep@gmail.com>
2013-12-23 13:01:34 +0900

    Implement connector_presenter_spec#commponents

--- spec/presenters/connector_presenter_spec.rb
+++ spec/presenters/connector_presenter_spec.rb
@@ -2,6 +2,19 @@ require 'spec_helper'
 
 module VhdlConnector::Presenters
   describe ConnectorPresenter do
+    describe '#commponents' do
+      subject { ConnectorPresenter.new.components(entity_path, options) }
+      let(:entity_path) { './examples/no_generics_entity.vhd' } 
+
+      context 'add no generic entity' do
+        let(:options) { Hash.new }
+
+        it do
+          expect(subject.size).to be(1)
+        end
+      end
+    end
+
     describe '#header_obj' do
       subject { ConnectorPresenter.new(entities: entities).header_obj }
       let(:entities) { entity_pathes.map { |path| VhdlConnector.parse_entity_file(path) } }
