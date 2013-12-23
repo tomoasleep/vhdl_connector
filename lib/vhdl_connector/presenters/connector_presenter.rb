724d12adcc3b0037e73e3986a3f09e3ca1ba7d39
Tomoya Chiba <tomo.asleep@gmail.com>
2013-12-23 12:37:19 +0900

    Implement component method (connector_presenter)

--- lib/vhdl_connector/presenters/connector_presenter.rb
+++ lib/vhdl_connector/presenters/connector_presenter.rb
@@ -12,18 +12,53 @@ module VhdlConnector::Presenters
       @cwd || './'
     end
 
-    def use_entity(*pathes)
-      @dependency_entities += pathes
+    def components(*args)
+      pathes = args_without_options(args)
+      options = options_of_args(args)
+
+      new_entities =  pathes
         .map { |path| File.expand_path(path, cwd) }
         .map { |abs_path| parse_entity_file(abs_path) }
+      @dependency_entities += new_entities
+
+      options.map do |key, value|
+        case key.to_sym
+        when :as
+          @aliases ||= {}
+          name_map = value.is_a? Hash ? value : { new_entities.first.name => value }
+          @aliases.merge name_map.symbolize_keys
+        end
+      end
     end
 
     def header(*options)
-      @dependency_entities.map { |entity| entity.to_component_definition }
+      header_obj(*options).join('\n')
     end
 
     def mapping(*options)
-      @dependency_entities.map { |entity| entity.to_component_mapping }
+      mapping_obj(*options).join('\n')
+    end
+
+    def header_obj(*options)
+      @dependency_entities.map { |entity| entity.to_component_definition }
+    end
+
+    def mapping_obj(*options)
+      @dependency_entities
+        .map { |entity| resolve_aliases(entity) }
+        .flatten
+        .map { |entity| entity.to_component_mapping }
+    end
+
+    private
+    def resolve_aliases(entity)
+      names = @aliases[entity.name.to_sym]
+      return entity unless names
+      case names
+      when Array
+        names.map { |name| EntityWrapper.new(entity.entity, local_name: name.to_s) }
+      else
+      end
     end
   end
 end
