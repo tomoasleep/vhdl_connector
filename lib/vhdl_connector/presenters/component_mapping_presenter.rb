3f48110fa759812d2a3cc6fb433f624931b1ff63
Tomoya Chiba <tomo.asleep@gmail.com>
2013-12-23 15:08:59 +0900

    Fix escape of component_mapping_presenter

--- lib/vhdl_connector/presenters/component_mapping_presenter.rb
+++ lib/vhdl_connector/presenters/component_mapping_presenter.rb
@@ -2,11 +2,11 @@ module VhdlConnector::Presenters
   class ComponentMappingPresenter < EntityPresenter
     alias :original_local_name :local_name
     def generic_mappings
-      generics.map { |g| g.mapping }.join(',\n')
+      generics.map { |g| g.mapping }.join(",\n")
     end
 
     def port_mappings
-      ports.map { |pt| "#{pt.name} => #{mapping_name_prefix}_#{pt.name}" }.join(',\n')
+      ports.map { |pt| "#{pt.name} => #{mapping_name_prefix}_#{pt.name}" }.join(",\n")
     end
 
     def local_name
