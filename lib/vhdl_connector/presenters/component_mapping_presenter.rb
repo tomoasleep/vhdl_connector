4c9a508deff734ea3ebab8adea7b9196b0bd96e3
Tomoya Chiba <tomo.asleep@gmail.com>
2013-12-23 12:34:13 +0900

    Use local_name to component_mapping

--- lib/vhdl_connector/presenters/component_mapping_presenter.rb
+++ lib/vhdl_connector/presenters/component_mapping_presenter.rb
@@ -1,12 +1,22 @@
 module VhdlConnector::Presenters
   class ComponentMappingPresenter < EntityPresenter
+    alias :original_local_name :local_name
     def generic_mappings
       generics.map { |g| g.mapping }.join(',\n')
     end
 
     def port_mappings
-      ports.map { |pt| "#{pt.name} => #{name}_#{pt.name}" }.join(',\n')
+      ports.map { |pt| "#{pt.name} => #{mapping_name_prefix}_#{pt.name}" }.join(',\n')
     end
+
+    def local_name
+      original_local_name || "#{name}_comp"
+    end
+
+    def mapping_name_prefix
+      original_local_name || name
+    end
+
   end
 end
 
