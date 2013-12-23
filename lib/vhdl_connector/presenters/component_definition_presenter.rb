94213c7b3b89adc93d4d5e5d72fdf7e8e6a684fa
Tomoya Chiba <tomo.asleep@gmail.com>
2013-12-23 15:08:34 +0900

    Fix escape of component_definition_presenter

--- lib/vhdl_connector/presenters/component_definition_presenter.rb
+++ lib/vhdl_connector/presenters/component_definition_presenter.rb
@@ -1,11 +1,11 @@
 module VhdlConnector::Presenters
   class ComponentDefinitionPresenter < EntityPresenter
     def generic_definitions
-      generics.map { |g| g.definition }.join(';\n')
+      generics.map { |g| g.definition }.join(";\n")
     end
 
     def port_definitions
-      ports.map { |pt| pt.definition }.join(';\n')
+      ports.map { |pt| pt.definition }.join(";\n")
     end
   end
 end
