74546f08a279f17c49775d9f9aa169d39d94597d
Tomoya Chiba <tomo.asleep@gmail.com>
2014-02-02 22:51:28 +0900

    Modify ComponentMapping's naming

--- lib/vhdl_connector/presenters/component_mapping_presenter.rb
+++ lib/vhdl_connector/presenters/component_mapping_presenter.rb
@@ -1,12 +1,18 @@
 module VhdlConnector::Presenters
   class ComponentMappingPresenter < EntityPresenter
     alias :original_local_name :local_name
+    def initialize(entity, options)
+      aliases = options[:as] if options && (options.is_a? Hash)
+      @aliases = aliases || {}
+      @entity = entity
+    end
+
     def generic_mappings
       generics.map { |g| g.mapping }.join(",\n")
     end
 
     def port_mappings
-      ports.map { |pt| "#{pt.name} => #{mapping_name_prefix}_#{pt.name}" }.join(",\n")
+      ports.map { |pt| "#{pt.name} => #{signal_name(pt)}" }.join(",\n")
     end
 
     def local_name
@@ -17,6 +23,10 @@ module VhdlConnector::Presenters
       original_local_name || name
     end
 
+    private
+    def signal_name(port)
+      (as = @aliases[port.name.to_sym]) ? as.to_s : "#{mapping_name_prefix}_#{port.name}"
+    end
   end
 end
 
