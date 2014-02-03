419a59b94cb939a86d998c8df91b2e0f605daa55
Tomoya Chiba <tomo.asleep@gmail.com>
2014-02-03 17:25:42 +0900

    Enable ConnectorReader to parse block line

--- lib/vhdl_connector/block_decorator.rb
+++ lib/vhdl_connector/block_decorator.rb
@@ -1,12 +1,21 @@
 module VhdlConnector
   module BlockDecorator
     extend self
+    COMPONENT_MAPPING_LABEL = /COMPONENT\s*MAPPING\s*BLOCK/
+    COMPONENT_DEFINITION_LABEL = /COMPONENT\s*DEFINITION\s*BLOCK/
+    SIGNAL_LABEL = /SIGNAL\s*BLOCK/
+
     COMPONENT_DEFINITION_BLOCK =
-      /^\s*--\s*COMPONENT\s*DEFINITION\s*BLOCK\s*BEGIN(.*?)--\s*COMPONENT\s*DEFINITION\s*BLOCK\s*END.*$/
+      /^\s*--\s*#{COMPONENT_DEFINITION_LABEL}(.*?)--\s*COMPONENT\s*DEFINITION\s*BLOCK\s*END.*$/
     COMPONENT_MAPPING_BLOCK =
-      /^\s*--\s*COMPONENT\s*MAPPING\s*BLOCK\s*BEGIN(.*?)--\s*COMPONENT\s*MAPPING\s*BLOCK\s*END.*$/
-    SIGNAL_BLOCK = /^\s*--\s*SIGNAL\s*BLOCK\s*BEGIN(.*?)--\s*SIGNAL\s*BLOCK\s*END.*$/
+      /^\s*--\s*#{COMPONENT_MAPPING_LABEL}\s*BEGIN(.*?)--\s*COMPONENT\s*MAPPING\s*BLOCK\s*END.*$/
+    SIGNAL_BLOCK = /^\s*--\s*#{SIGNAL_LABEL}\s*BEGIN(.*?)--\s*SIGNAL\s*BLOCK\s*END.*$/
+
     RUBY_BLOCK = /^(-- )?(.*)<%(.*?)%>/
+
+    COMPONENT_MAPPING_LINE = /#{COMPONENT_MAPPING_LABEL}\s*$/
+    COMPONENT_DEFINITION_LINE = /#{COMPONENT_DEFINITION_LABEL}\s*$/
+    SIGNAL_LINE = /#{SIGNAL_LABEL}\s*$/
 
     def decorate_component_definition_block(block)
       "-- COMPONENT DEFINITION BLOCK BEGIN {{{\n#{block}\n-- COMPONENT DEFINITION BLOCK END }}}"
@@ -26,8 +35,20 @@ module VhdlConnector
       end
     end
 
+    def undecorate_component_definition_line(code)
+      code.gsub(COMPONENT_DEFINITION_LINE) do |text|
+        %q(<%= component_definitions %>)
+      end
+    end
+
     def undecorate_component_mapping_block(code)
       code.gsub(COMPONENT_MAPPING_BLOCK) do |text|
+        %q(<%= component_mappings %>)
+      end
+    end
+
+    def undecorate_component_mapping_line(code)
+      code.gsub(COMPONENT_MAPPING_LINE) do |text|
         %q(<%= component_mappings %>)
       end
     end
@@ -38,11 +59,28 @@ module VhdlConnector
       end
     end
 
+    def undecorate_signal_definition_line(code)
+      code.gsub(SIGNAL_LINE) do |text|
+        %q(<%= component_signal_definitions %>)
+      end
+    end
+
     def escape_ruby(text)
       text.gsub(RUBY_BLOCK) do |code|
         code = $3
         %Q(-- #{$2}<%%#{code}%>\n<%#{code}%>)
       end
+    end
+
+    def undecorate_code(text)
+      text = BlockDecorator.escape_ruby(text)
+      text = BlockDecorator.undecorate_component_mapping_line(text)
+      text = BlockDecorator.undecorate_component_definition_line(text)
+      text = BlockDecorator.undecorate_signal_definition_line(text)
+      text = BlockDecorator.undecorate_component_mapping_block(text)
+      text = BlockDecorator.undecorate_component_definition_block(text)
+      text = BlockDecorator.undecorate_signal_definition_block(text)
+      text
     end
   end
 end
--- lib/vhdl_connector/connector_reader.rb
+++ lib/vhdl_connector/connector_reader.rb
@@ -2,11 +2,8 @@ module VhdlConnector
   class ConnectorReader
     attr_reader :body
 
-    def read(text)
-      body = BlockDecorator.escape_ruby(text)
-      body = BlockDecorator.undecorate_component_mapping_block(body)
-      body = BlockDecorator.undecorate_component_definition_block(body)
-      @body = BlockDecorator.undecorate_signal_definition_block(body)
+    def read(code)
+      @body = BlockDecorator.undecorate_code(code)
       self
     end
   end
